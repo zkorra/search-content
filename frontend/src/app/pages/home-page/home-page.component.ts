678254990f71850f7c593490df5d27ce90a8de57
zkorra <korrawich.dev@outlook.com>
2021-03-15 17:12:27 +0700

    init home

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ContentState } from '../../search/search.state';
+import { SearchContent } from '../../search/search.action';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  @Select(ContentState.getContent)
+  content: any;
 
-  constructor() { }
+  userParams = {
+    type: 'article',
+    cx: '07f7a2e8b0b662f50',
+    query: 'python',
+    // page: '',
+    // region: '',
+  };
 
-  ngOnInit(): void {
+  cols: any[] = [];
+
+  constructor(private store: Store) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.store.dispatch(new SearchContent(this.userParams)).toPromise();
   }
-
 }
