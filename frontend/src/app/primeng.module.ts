a133dc4c96af5ddb8d3d165e024828e85e57bf07
knwch <t.korrawich@protonmail.com>
2021-03-15 15:47:01 +0700

    add ui framework

---
+++ frontend/src/app/primeng.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { OrderListModule } from 'primeng/orderlist';
+import { DataViewModule } from 'primeng/dataview';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { CardModule } from 'primeng/card';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    OrderListModule,
+    DataViewModule,
+    AutoCompleteModule,
+    DialogModule,
+    DividerModule,
+    CardModule,
+  ],
+})
+export class PrimeNgModule {}
