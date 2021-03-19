af4a6a73983f49597174686796f24ff923a3dbcd
zkorra <korrawich.dev@outlook.com>
2021-03-19 15:31:44 +0700

    changed args name

--- backend/custom_search_engine.py
+++ backend/custom_search_engine.py
@@ -4,19 +4,19 @@ GOOGLE_API_KEY = "AIzaSyB6trExhF-mxOKdNQsAW-HkVOHBGV8W0R4"
 COUNT = 0
 
 
-def fetchCustomSearch(cx, query, page, region):
+def fetchCustomSearch(searchEngineId, keyword, page, region):
 
     REGION_PARAM = ""
 
     if region:
         REGION_PARAM = f"&cr={region}"
 
-    SEARCH_ENGINE_ID = cx
+    SEARCH_ENGINE_ID = searchEngineId
 
     # calculating start, (page=2) => (start=11), (page=3) => (start=21)
     start = (int(page) - 1) * 10 + 1
 
-    url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}{REGION_PARAM}"
+    url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={keyword}&start={start}{REGION_PARAM}"
 
     data = requests.get(url).json()
 
