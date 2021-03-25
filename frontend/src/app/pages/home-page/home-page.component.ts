a05e6d680a7976c642263034b9978608d2f315a0
knwch <t.korrawich@protonmail.com>
2021-03-25 16:11:27 +0700

    fixed url

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -31,7 +31,7 @@ export class HomePageComponent implements OnInit {
       title: '15 Free Python Courses for Beginners to Learn Online',
       description:
         'A curated list of some of the free online courses to learn Python.',
-      cse_url:
+      url:
         'https://medium.com/swlh/5-free-python-courses-for-beginners-to-learn-online-e1ca90687caf',
       image: 'https://miro.medium.com/max/840/1*RJMxLdTHqVBSijKmOO5MAg.jpeg',
       date: '12/08/2020',
@@ -45,7 +45,7 @@ export class HomePageComponent implements OnInit {
       title: 'Python: 7 Important Reasons Why You Should Use Python',
       description:
         'According to the latest TIOBE Programming Community Index, Python is one of the top 10 popular programming languages of 2017. Python is a\u2026',
-      cse_url:
+      url:
         'https://medium.com/@mindfiresolutions.usa/python-7-important-reasons-why-you-should-use-python-5801a98a0d0b',
       image: 'https://miro.medium.com/max/1140/1*WizgUsFeUgISS7vkFl4dEA.jpeg',
       date: '10/03/2017',
@@ -59,7 +59,7 @@ export class HomePageComponent implements OnInit {
       title: '12 Websites to Learn Python Programming for FREE',
       description:
         'Hello guys, if you are here then let me first congratulate you for making the right decision to learn Python programming language, the\u2026',
-      cse_url:
+      url:
         'https://medium.com/javarevisited/10-free-python-tutorials-and-courses-from-google-microsoft-and-coursera-for-beginners-96b9ad20b4e6',
       image: 'https://miro.medium.com/max/840/1*RJMxLdTHqVBSijKmOO5MAg.jpeg',
       date: '12/09/2020',
@@ -73,7 +73,7 @@ export class HomePageComponent implements OnInit {
       title: 'Python at Netflix',
       description:
         'By Pythonistas at Netflix, coordinated by Amjith Ramanujam and edited by Ellen Livengood',
-      cse_url:
+      url:
         'https://medium.com/netflix-techblog/python-at-netflix-bba45dae649e',
       image: 'https://miro.medium.com/max/601/1*PPIp7twJJUknfohZqtL8pQ.png',
       date: '30/04/2019',
@@ -87,7 +87,7 @@ export class HomePageComponent implements OnInit {
       title: 'Python Lists and List Manipulation',
       description:
         'Before starting, I should mention that the code in this blog post and in the video above is available on my github.',
-      cse_url:
+      url:
         'https://medium.com/@GalarnykMichael/python-basics-6-lists-and-list-manipulation-a56be62b1f95',
       image: 'https://miro.medium.com/max/310/1*noxS-kGyde8dhaTXfKMTKg.png',
       date: '11/12/2019',
@@ -101,7 +101,7 @@ export class HomePageComponent implements OnInit {
       title: 'code_swarm - Python',
       description:
         'Visualizing the commit history of the Python scripting language project.  Project page: http://vis.cs.ucdavis.edu/~ogawa/codeswarm/  Open source: http://code.google.com/p/codeswarm  Music: "Overflight"\u2026',
-      cse_url: 'https://medium.com/media/176efd03c2b4aac3c843fc47c3fa8515/href',
+      url: 'https://medium.com/media/176efd03c2b4aac3c843fc47c3fa8515/href',
       image:
         'https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F21009317_1280x965.jpg&src1=https%3A%2F%2Ff.vimeocdn.com%2Fimages_v6%2Fshare%2Fplay_icon_overlay.png',
       date: '30/05/2008',
@@ -115,7 +115,7 @@ export class HomePageComponent implements OnInit {
       title: 'Python at Netflix',
       description:
         'Python\u2019s footprint in our environment continues to increase',
-      cse_url:
+      url:
         'https://medium.com/netflix-techblog/python-at-netflix-86b6028b3b3e',
       image: '',
       date: '20/09/2018',
@@ -129,7 +129,7 @@ export class HomePageComponent implements OnInit {
       title: 'PCA using Python (scikit-learn)',
       description:
         'My last tutorial went over Logistic Regression using Python. One of the things learned was that you can speed up the fitting of a machine learning algorithm by changing the optimization algorithm. A\u2026',
-      cse_url:
+      url:
         'https://medium.com/@GalarnykMichael/pca-using-python-scikit-learn-e653f8989e60',
       image: 'https://miro.medium.com/max/1050/1*Gob8ZbScyM7hHUHjvrMJYg.png',
       date: '02/03/2021',
@@ -143,7 +143,7 @@ export class HomePageComponent implements OnInit {
       title: 'Hypermodern Python',
       description:
         'A guide to modern Python tooling with a focus on simplicity and minimalism. This chapter covers setting up a project using Poetry and\u2026',
-      cse_url: 'https://medium.com/@cjolowicz/hypermodern-python-d44485d9d769',
+      url: 'https://medium.com/@cjolowicz/hypermodern-python-d44485d9d769',
       image: 'https://miro.medium.com/max/1200/1*F1PgdlB3Ez-xek3oU7fQzw.jpeg',
       date: '26/03/2020',
       meta_article_author: 'https://medium.com/@cjolowicz',
@@ -156,7 +156,7 @@ export class HomePageComponent implements OnInit {
       title: 'Building A Logistic Regression in Python, Step by Step',
       description:
         'Logistic Regression is a Machine Learning classification algorithm that is used to predict the probability of a categorical dependent\u2026',
-      cse_url:
+      url:
         'https://medium.com/towards-data-science/building-a-logistic-regression-in-python-step-by-step-becd4d56c9c8',
       image: 'https://miro.medium.com/max/1163/1*LrA_HrggXM_BDfippYTVxQ.png',
       date: '27/02/2019',
@@ -207,7 +207,7 @@ export class HomePageComponent implements OnInit {
     });
 
     // this.allColumns = [
-    //   ...this.sswarpContent.reduce(
+    //   ...this.warpContent.reduce(
     //     (set, object) => (
     //       Object.keys(object).forEach((key) => set.add(key)), set
     //     ),
@@ -308,5 +308,9 @@ export class HomePageComponent implements OnInit {
       this.allColumns = warpColumns;
       this.warpSelectedColumns = this.allColumns;
     }
+  }
+
+  goToExternalWebsite(url: any): void {
+    window.open(url, '_blank');
   }
 }
