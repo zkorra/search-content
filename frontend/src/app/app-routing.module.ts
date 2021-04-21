7300b520d998a5ab93865260f87e8fe88ec399c8
zkorra <korrawich.dev@outlook.com>
2021-04-21 17:22:39 +0700

    new route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
+  { path: 'history', component: HistoryPageComponent },
 ];
 
 @NgModule({
