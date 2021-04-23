6168b0cbe525edf98d1398988906c145dae5242f
zkorra <korrawich.dev@outlook.com>
2021-04-23 17:54:02 +0700

    loading state

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  actionsExecuting,
+  ActionsExecuting,
+} from '@ngxs-labs/actions-executing';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import {
+  SearchContent,
+  GetHistory,
+  GetContentFile,
+  DeleteHistory,
+  SaveSelectedContent,
+} from './search/search.action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  @Select(
+    actionsExecuting([
+      SearchContent,
+      GetHistory,
+      GetContentFile,
+      DeleteHistory,
+      SaveSelectedContent,
+    ])
+  )
+  ActionsIsExecuting$!: Observable<ActionsExecuting>;
 }
